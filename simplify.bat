4c5fdafda4fec5912e68e11f974338ac058ba4a0
Yueyu <gaoboyuan8@qq.com>
2018-05-08 21:51:38 +0800

    Update.

--- simplify.bat
+++ simplify.bat
@@ -2,8 +2,8 @@
 rem You can delete all lines that contains unrecognizable words if your system language is not Chinese.
 
 set py=python
-%py% --version>nul 2>nul||set py=py
-%py% --version>nul 2>nul||echo 没有找到 Python.&goto:eof
+(%py% --version>nul 2>nul)||(set py=py)
+(%py% --version>nul 2>nul)||(echo 没有找到 Python.&goto:eof)
 
 if "%~1"=="" (
 	echo 命令行：%0 ^<文件名^>
@@ -13,10 +13,10 @@ if "%~1"=="" (
 	goto:eof
 )
 md tmp
-python imdiv.py --img "%~1" --out_fmt tmp/div_%%d_%%d.png
+%py% imdiv.py --img "%~1" --out_fmt tmp/div_%%d_%%d.png
 set count_y=0
 for %%i in (tmp\div_*.png) do (
-	python simplify.py --img %%i --out tmp/out_%%~nxi
+	%py% simplify.py --img %%i --out tmp/out_%%~nxi
 	set /a count_y+=1
 )
 set count_x=0
@@ -24,5 +24,5 @@ for %%i in (tmp\div_0_*.png) do (
 	set /a count_x+=1
 )
 set /a count_y/=count_x
-python imcmb.py --img_fmt tmp/out_div_%%d_%%d.png --out "%~dp1/out_%~nx1" --nx %count_x% --ny %count_y%
+%py% imcmb.py --img_fmt tmp/out_div_%%d_%%d.png --out "%~dp1/out_%~nx1" --nx %count_x% --ny %count_y%
 rd /s /q tmp
